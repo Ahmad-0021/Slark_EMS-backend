7259422a27bebfb7ee310aa72952cd7ae2c0bfda
Muhammad Ahmad <ah167493991@gmail.com>
2025-01-06 19:23:00 +0500

    some fixes

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: `${process.env.CORS_ORIGIN || 'https://slark-ems-frontend.vercel.app'}`, // Allowed frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'], // Headers allowed
     credentials: true, // Allow cookies and authorization headers
-    preflightContinue: false, // Continue with OPTIONS request
+    // Continue with OPTIONS request
   });
 
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
