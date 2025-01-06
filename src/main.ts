7fa1b7878c53a157c99cfd2625fc41195f05c3cf
Muhammad Ahmad <ah167493991@gmail.com>
2025-01-06 19:00:19 +0500

    optional origin

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AuthGuard } from './common/guard/auth.guard';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: `${process.env.CORS_ORIGIN}`, // Allowed frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: `${process.env.CORS_ORIGIN || 'https://slark-ems-frontend.vercel.app'}`, // Allowed frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: ['Content-Type', 'Authorization'], // Headers allowed
     credentials: true, // Allow cookies and authorization headers
     preflightContinue: false, // Continue with OPTIONS request
