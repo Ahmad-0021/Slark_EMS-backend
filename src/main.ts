e30bc2595ac7c36924c23b0ba79d4df1676a3f86
Muhammad Ahmad <ah167493991@gmail.com>
2025-01-06 13:51:31 +0500

    fixes

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: 'https://slark-ems-frontend.vercel.app', // Allow specific origins
+    origin: 'https://slark-ems-frontend.vercel.app', // Allowed frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed methods
-    credentials: true, // Allow cookies
+    allowedHeaders: ['Content-Type', 'Authorization'], // Headers allowed
+    credentials: true, // Allow cookies and authorization headers
+    preflightContinue: false, // Continue with OPTIONS request
   });
+
   await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
